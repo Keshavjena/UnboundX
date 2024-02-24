927ca10f40e1097a6503e67baaba5cece6df8f4b
Keshav Jena <keshav29903@gmail.com>
2024-02-24 23:24:45 +0530

    File Setup

--- frontend/src/Containers/index.js
+++ frontend/src/Containers/index.js
@@ -1,1 +1,15 @@
 import Header from './Header/Header'
+import About from './About/About'
+import Contact from './Contact/Contact'
+import Explore from './Explore/Explore'
+import Login from './Login/Login'
+import Signup from './Signup/Signup'
+
+export {
+    Header,
+    About,
+    Contact,
+    Explore,
+    Login,
+    Signup
+}
